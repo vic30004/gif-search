bc4f1f4d118fc14e2b9cedd87935e9e5b1c61b5d
vic30004 <victor_993@msn.com>
2019-07-28 11:48:09 -0700

    btns will display  gifs and their ratings. Clicking on gifs will animate them.

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -2,33 +2,68 @@ let savedBtn = ["007", "wwe", "fails", "cats", "dogs",]
 
 function displayMovieInfo() {
 
-    let movie = $(this).attr("data-name");
-    let queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(queryURL);
-      console.log(JSON.stringify(response))
-      $("#movies-view").text(JSON.stringify(response));
-    });
-  }
 
 
-  function buttonRender (){
     $(".search-btn").empty();
-    for (let i=0; i<savedBtn.length; i++){
-        let a= $("<button>");
-        a.addClass("movie-btn");
+    for (let i = 0; i < savedBtn.length; i++) {
+        let a = $("<button>");
+        a.addClass("gif-btn");
         a.attr("data-name", savedBtn[i]);
         a.text(savedBtn[i]);
         $(".search-btn").append(a);
     }
-  }
+    $(".search-btn").on("click", ".gif-btn", function () {
+        $(".gif-section").empty();
+        let gifTitle = $(this).attr("data-name");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            gifTitle + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            let results = response.data;
+
+            for (let i = 0; i < results.length; i++) {
+                let gifDiv = $("<div>");
+                let rating = results[i].rating;
+                let p = $("<p>").text("Rating: " + rating);
+                let still = results[i].images.fixed_height_still.url
+                let animate = results[i].images.fixed_height.url
+                let img = $("<img>");
+                img.attr("src", still);
+                img.attr("data-still", still);
+                img.attr("data-animate", animate)
+                img.attr("data-state", "still")
+                img.addClass("gif")
+                gifDiv.append(p);
+                gifDiv.append(img);
+                $(".gif-section").prepend(gifDiv);
+
+                $(".gif").on("click", function () {
+                    let state = $(this).attr("data-state");
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                })
+            }
+        })
+    });
+}
 
 
 
 
-  
-  buttonRender();
+
+$("#add-gif").on("click", function () {
+    event.preventDefault();
+    let newGif = $("#gif-input").val().trim();
+    savedBtn.push(newGif)
+    displayMovieInfo()
+})
+displayMovieInfo()
