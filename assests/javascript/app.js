0ca883e3aa7aa4e5fe16aa9c49ad7df13c0365ee
vic30004 <victor_993@msn.com>
2019-07-29 00:15:39 -0700

    fixed an error where if a btn was added through search, it was being duplicated

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,19 +1,8 @@
-let imported = document.createElement('script');
-imported.src = '/assests/javascript/keys.js';
-document.head.appendChild(imported);
 
 let savedBtn = ["007", "wwe", "fails", "cats", "dogs",]
 
 function displayMovieInfo() {
 
-    $(".search-btn").empty();
-    for (let i = 0; i < savedBtn.length; i++) {
-        let a = $("<button>");
-        a.addClass("gif-btn");
-        a.attr("data-name", savedBtn[i]);
-        a.text(savedBtn[i]);
-        $(".search-btn").append(a);
-    }
     $(".search-btn").on("click", ".gif-btn", function () {
         $("#gif-section").empty();
         let gifTitle = $(this).attr("data-name");
@@ -24,26 +13,25 @@ function displayMovieInfo() {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
+            console.log(response);
             let results = response.data;
 
             for (let i = 0; i < results.length; i++) {
                 let gifDiv = $("<div>");
                 let rating = results[i].rating;
                 let p = $("<p class='rate'>").text("Rating: " + rating);
-                let still = results[i].images.fixed_height_still.url
-                let animate = results[i].images.fixed_height.url
-
+                let still = results[i].images.fixed_height_still.url;
+                let animate = results[i].images.fixed_height.url;
                 let img = $("<img>");
                 img.attr("src", still);
                 img.attr("data-still", still);
-                img.attr("data-animate", animate)
-                img.attr("data-state", "still")
-                img.addClass("gif")
+                img.attr("data-animate", animate);
+                img.attr("data-state", "still");
+                img.addClass("gif");
                 gifDiv.append(p);
                 gifDiv.append(img);
                 $("#gif-section").prepend(gifDiv);
-            }
+         }   
 
 
 
@@ -55,14 +43,13 @@ function displayMovieInfo() {
                     $(this).attr("src", $(this).attr("data-animate"));
                     $(this).attr("data-state", "animate");
                     anime = true;
-                } if (anime === false) {
+                } if (!anime) {
                     $(this).attr("src", $(this).attr("data-still"));
                     $(this).attr("data-state", "still");
                     
-                }
-
-                
+                } 
             })
+            
         })
     });
 }
@@ -70,7 +57,21 @@ function displayMovieInfo() {
 $("#add-gif").on("click", function () {
     event.preventDefault();
     let newGif = $("#gif-input").val().trim();
-    savedBtn.push(newGif)
-    displayMovieInfo()
+    savedBtn.push(newGif);
+addBtn();
 })
-displayMovieInfo()
+
+function addBtn(){
+$(".search-btn").empty();
+for (let i = 0; i < savedBtn.length; i++) {
+    let a = $("<button>");
+    a.addClass("gif-btn");
+    a.attr("data-name", savedBtn[i]);
+    a.text(savedBtn[i]);
+    $(".search-btn").append(a);
+   
+}
+}
+addBtn();
+
+displayMovieInfo();
